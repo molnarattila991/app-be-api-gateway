65cc023b21a2090ae2a2f5d8893152510158417a
molnattila991 <molnarattila991@gmail.com>
2022-07-11 09:59:45 +0200

    Containereize app

--- src/services/servcies.module.ts
+++ src/services/servcies.module.ts
@@ -3,7 +3,7 @@ import { UserProducerService } from './user-producer/user-producer.service';
 import { BusModule } from "moat-lib-be-pubsub/pub-sub";
 @Module({
     providers: [
-        BusModule.initPublisher("localhost:50000"),
+        BusModule.initPublisher(process.env["NEST_MQ_CONNECTION_STRING"]),
         UserProducerService
     ],
     exports: [
